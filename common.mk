75397cf6e40f73ece4c21b4941b5fa5ea0c4a8b5
4mede <4mede@tutamail.com>
2025-03-05 17:21:45 -0500

    MotoFm: initial commit

---
+++ common.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2008 - 2025 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# SOONG NAMESPACES
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/motorola/MotoFm
+
+# Properties
+TARGET_SYSTEM_PROP += \
+    vendor/motorola/MotCamera-common/proprietary/system/build.prop
