084b76e89c399566559aa5b62962389827c90cdc
Vachounet <vachounet@live.fr>
2025-05-01 20:46:04 -0400

    MotoFm: Redo the repo
    Co-authored-by: Murat Kozan <muratkozan350@gmail.com>
    Co-authored-by: daniml3 <danimoral1001@gmail.com>
    Co-authored-by: David <davidmolinamorales1999@gmail.com>
    Co-authored-by: Deivid21 <david.parra.ignacio@gmail.com>
    Change-Id: I1279c1b9f94c48c241a0338f6053de00d0b5f86f

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+To use this repo, just add the following to device.mk
+```
 # MotoFm
+$(call inherit-product, vendor/motorola/MotoFm/common.mk)
+```
+
+Then clone the necessary repos:
+
+```
+git clone -b main https://github.com/4mede/MotoFm.git vendor/motorola/MotoFm
+```
+
+Moto Signature App repo: (needed to update moto apps in Play Store)
+```
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotoSignatureApp.git -b android-15 vendor/motorola/MotoSignatureApp
+```
+
+Motorola Settings Provider repo: (this will make the application works)
+```
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotorolaSettingsProvider.git -b android-15 vendor/motorola/MotorolaSettingsProvider
+```
+
--- common.mk
+++ common.mk
@@ -1,23 +1,43 @@
-#
-# Copyright (C) 2008 - 2025 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 # SOONG NAMESPACES
 PRODUCT_SOONG_NAMESPACES += \
-    vendor/motorola/MotoFm
+    vendor/motorola/MotoFm \
+    vendor/qcom/opensource/commonsys/fm
 
-# Properties
-TARGET_SYSTEM_PROP += \
-    vendor/motorola/MotCamera-common/proprietary/system/build.prop
+BOARD_HAVE_QCOM_FM := true
+BOARD_HAS_QCA_FM_SOC := "cherokee"
+
+PRODUCT_PACKAGES += \
+    libqcomfm_jni \
+    qcom.fmradio \
+    motorola.hardware.fmradio \
+    motorola.hardware.fmradio@1.0-service \
+    init.qti.fm.rc \
+    init.qti.fm.sh \
+    FMRadioService \
+    FMPlayer
+    
+PRODUCT_BOOT_JARS += qcom.fmradio
+  
+PRODUCT_COPY_FILES += \
+     $(call find-copy-subdir-files,*,vendor/motorola/MotoFm/prebuilt/permissions,product/etc/permissions)
+  
+AB_OTA_PARTITIONS += \
+    product
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    vendor/motorola/MotoFm/sepolicy/vendor
+
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/motorola/MotoFm/framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += vendor/motorola/MotoFm/manifest.xml
+
+PRODUCT_COPY_FILES += \
+    vendor/motorola/MotoFm/prebuilt/init/init.qti.fm.sh:$(TARGET_COPY_OUT_VENDOR)/etc/init.qti.fm.sh \
+    vendor/motorola/MotoFm/prebuilt/init/init.qti.fm.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qti.fm.rc \
+
+
+
+
+
+
