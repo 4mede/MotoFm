039c7c594e676feecd579cb3826ac8ba281ea159
4mede <4mede@tutamail.com>
2025-05-05 10:19:47 -0400

    MotoFm: some fixes
    - correctly build motorola.hardware.fmradio
    - include signatureapp and settingsprovider
    
    Change-Id: I0638cc8bfabbf3497776da1b004f82d31a4be7ff

--- common.mk
+++ common.mk
@@ -1,21 +1,34 @@
-# SOONG NAMESPACES
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/motorola/MotoFm \
-    vendor/qcom/opensource/commonsys/fm
-
+# Flags
 BOARD_HAVE_QCOM_FM := true
 BOARD_HAS_QCA_FM_SOC := "cherokee"
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/motorola/MotoFm/framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += vendor/motorola/MotoFm/manifest.xml
+
+PRODUCT_COPY_FILES += \
+    vendor/motorola/MotoFm/prebuilt/init/init.qti.fm.sh:$(TARGET_COPY_OUT_VENDOR)/etc/init.qti.fm.sh \
+    vendor/motorola/MotoFm/prebuilt/init/init.qti.fm.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qti.fm.rc \
+
+# Moto Signature App
+$(call inherit-product, vendor/motorola/MotoSignatureApp/motosignatureapp.mk)
+
+# Motorola Settings Provider
+$(call inherit-product, vendor/motorola/MotorolaSettingsProvider/motorolasettingsprovider.mk)
+
+# Packages
 PRODUCT_PACKAGES += \
     libqcomfm_jni \
     qcom.fmradio \
-    motorola.hardware.fmradio \
+    motorola.hardware.fmradio@1.0 \
     motorola.hardware.fmradio@1.0-service \
     FMRadioService \
     FMPlayer
     
 PRODUCT_BOOT_JARS += qcom.fmradio
   
+# Permissions
+
 PRODUCT_COPY_FILES += \
      $(call find-copy-subdir-files,*,vendor/motorola/MotoFm/prebuilt/permissions,product/etc/permissions)
   
@@ -26,13 +39,10 @@ AB_OTA_PARTITIONS += \
 BOARD_VENDOR_SEPOLICY_DIRS += \
     vendor/motorola/MotoFm/sepolicy/vendor
 
-# HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/motorola/MotoFm/framework_compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += vendor/motorola/MotoFm/manifest.xml
-
-PRODUCT_COPY_FILES += \
-    vendor/motorola/MotoFm/prebuilt/init/init.qti.fm.sh:$(TARGET_COPY_OUT_VENDOR)/etc/init.qti.fm.sh \
-    vendor/motorola/MotoFm/prebuilt/init/init.qti.fm.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qti.fm.rc \
+# SOONG NAMESPACES
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/motorola/MotoFm \
+    vendor/qcom/opensource/commonsys/fm
 
 
 
